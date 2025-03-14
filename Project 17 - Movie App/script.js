55f6e51d7bd8cd0d30fc1e4821a60c7ee807d677
John Ritter <john.ritter.8788@gmail.com>
2025-03-14 10:29:44 -0400

    Revised file issues

--- Project 17 - Movie App/script.js
+++ Project 17 - Movie App/script.js
@@ -1,3 +1,8 @@
+const key1 = config.MOVIE_KEY;
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=' + key1;
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=' + key1;
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
