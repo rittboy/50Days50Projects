8d955e0a24c554dd494d9d46e2445219bfb633f3
John Ritter <john.ritter.8788@gmail.com>
2025-05-16 13:17:10 -0400

    Finished coding project 34 adjusting styling issues

--- Project 34 - Animated Countdown/script.js
+++ Project 34 - Animated Countdown/script.js
@@ -5,6 +5,36 @@ const replay = document.querySelector("#replay");
 
 runAnimation();
 
-function runAnimation() {
-  nums.forEach();
+function resetDOM() {
+  counter.classList.remove("hide");
+  finalMessage.classList.remove("show");
+
+  nums.forEach((num) => {
+    num.classList.value = "";
+  });
+
+  nums[0].classList.add("in");
 }
+
+function runAnimation() {
+  nums.forEach((num, idx) => {
+    const nextToLast = nums.length - 1;
+
+    num.addEventListener("animationend", (e) => {
+      if (e.animationName === "goIn" && idx !== nextToLast) {
+        num.classList.remove("in");
+        num.classList.add("out");
+      } else if (e.animationName === "goOut" && num.nextElementSibling) {
+        num.nextElementSibling.classList.add("in");
+      } else {
+        counter.classList.add("hide");
+        finalMessage.classList.add("show");
+      }
+    });
+  });
+}
+
+replay.addEventListener("click", () => {
+  resetDOM();
+  runAnimation();
+});
