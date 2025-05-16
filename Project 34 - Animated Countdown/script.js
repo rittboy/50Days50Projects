820e335f7c0ee132a5bdc9eb3ebef9b3e7c8513a
John Ritter <john.ritter.8788@gmail.com>
2025-05-16 13:00:34 -0400

    FInished styling started coding project 34

---
+++ Project 34 - Animated Countdown/script.js
@@ -0,0 +1,10 @@
+const nums = document.querySelectorAll(".nums span");
+const counter = document.querySelector(".counter");
+const finalMessage = document.querySelector(".final");
+const replay = document.querySelector("#replay");
+
+runAnimation();
+
+function runAnimation() {
+  nums.forEach();
+}
