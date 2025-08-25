fa3e4e1218884c7b02069927cc1a939bdb6cd3cd
John Ritter <john.ritter.8788@gmail.com>
2025-08-25 11:00:53 -0400

    Finished project 42

---
+++ Project 42 - Live User Filter/script.js
@@ -0,0 +1,33 @@
+const result = document.getElementById("result");
+const filter = document.getElementById("filter");
+getData();
+filter.addEventListener("input", (e) => filterData(e.target.value));
+const listItems = [];
+async function getData() {
+  const res = await fetch("https://randomuser.me/api?results=50");
+  const { results } = await res.json();
+  //   console.log(data);
+  result.innerHTML = "";
+  results.forEach((user) => {
+    const li = document.createElement("li");
+    listItems.push(li);
+    li.innerHTML = `
+    <img src="${user.picture.large}" alt="${user.name.first}">
+    <div class="user-info">
+    <h4>${user.name.first} ${user.name.last}</h4>
+    <p>${user.location.city}, ${user.location.country}</p>
+    </div>
+
+    `;
+    result.appendChild(li);
+  });
+}
+function filterData(searchTerm) {
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm)) {
+      item.classList.remove("hide");
+    } else {
+      item.classList.add("hide");
+    }
+  });
+}
