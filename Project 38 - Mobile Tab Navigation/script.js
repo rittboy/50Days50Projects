1e596dc3cd5c24bdf1cd669fb4a0f57a208d784f
John Ritter <john.ritter.8788@gmail.com>
2025-08-25 08:01:38 -0400

    Finished fixing issue with display of pictures, and scripting to allow change between pictures

--- Project 38 - Mobile Tab Navigation/script.js
+++ Project 38 - Mobile Tab Navigation/script.js
@@ -1,16 +1,14 @@
 const contents = document.querySelectorAll(".content");
 const listItems = document.querySelectorAll("nav ul li");
-
 listItems.forEach((item, idx) => {
   item.addEventListener("click", () => {
     hideAllContents();
     hideAllItems();
 
     item.classList.add("active");
-    contents[idx].classList.add("active");
+    contents[idx].classList.add("show");
   });
 });
-
 function hideAllContents() {
   contents.forEach((content) => content.classList.remove("show"));
 }
