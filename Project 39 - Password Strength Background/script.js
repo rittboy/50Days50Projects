0bcfa00afab8d4ce7bbb3522b2dc7f37521547a3
John Ritter <john.ritter.8788@gmail.com>
2025-08-25 09:34:26 -0400

    Started Project 39

---
+++ Project 39 - Password Strength Background/script.js
@@ -0,0 +1,9 @@
+const password = document.getElementById("password");
+const background = document.getElementById("background");
+
+password.addEventListener("input", (e) => {
+  const input = e.target.value;
+  //   console.log(input);
+  const length = input.length;
+  //   console.log(20 - length * 2);
+});
