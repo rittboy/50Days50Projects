d0bf7e801a282f1559c323b2aeff11ab4c512473
John Ritter <john.ritter.8788@gmail.com>
2026-03-01 00:18:40 -0500

    Finished project 43, fixed issue with feedback displaying properly

--- Project 43 - Feedback UI Design/script.js
+++ Project 43 - Feedback UI Design/script.js
@@ -1,3 +1,37 @@
 const ratings = document.querySelectorAll(".rating");
-const send = document.querySelector("#send");
+const sendBtn = document.querySelector("#send");
 const panel = document.querySelector("#panel");
+const ratingsContainer = document.querySelector(".ratings-container");
+let selectedRating = "Satisfied";
+ratingsContainer.addEventListener("click", (e) => {
+  if (
+    e.target.parentNode.classList.contains("rating") &&
+    e.target.nextElementSibling
+  ) {
+    removeActive();
+    e.target.parentNode.classList.add("active");
+    selectedRating = e.target.nextElementSibling.innerHTML;
+  } else if (
+    e.target.parentNode.classList.contains("rating") &&
+    e.target.previousSibling &&
+    e.target.previousSibling.nodeName === "IMG"
+  ) {
+    removeActive();
+    e.target.parentNode.classList.add("active");
+    selectedRating = e.target.innerHTML;
+  }
+});
+
+sendBtn.addEventListener("click", (e) => {
+  panel.innerHTML = `<i class="fas fa-heart"></i>
+    <strong>Thank You!</strong>
+    <br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <p>We'll use your feedback to improve our customer support</p>`;
+});
+
+function removeActive() {
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove("active");
+  }
+}
